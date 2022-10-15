8973bdf0151bfc67aab90d56c2b1a2cc74030130
James Goodman <jamesgoodman206@gmail.com>
2022-10-15 22:35:46 +0100

    completed 3rd kata

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,6 @@
-const numberToReversedDigits = (number) => {};
-
+const numberToReversedDigits = (number) => {
+  const newNumbers = number.toString().split("");
+  newNumbers.reverse();
+  return newNumbers.map((numberAsString) => Number(numberAsString));
+};
 module.exports = numberToReversedDigits;
