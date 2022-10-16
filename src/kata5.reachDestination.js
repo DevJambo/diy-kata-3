45362c65fcd9cd53fbe2079f474d3cc45b436160
James Goodman <jamesgoodman206@gmail.com>
2022-10-16 20:31:46 +0100

    completed kata 5 Reach Destination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const arrivalTime = distance / speed;
+  const roundedArrivalTime = Math.round(arrivalTime / 0.5) * 0.5;
+  return `I should be there in ${roundedArrivalTime} hours`;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours");
+
+    expect(reachDestination(58, 25)).toEqual("I should be there in 2.5 hours");
+  });
 });
