d17d4dc0d1a776d880169de303ad7a02c6c03bdb
James Goodman <jamesgoodman206@gmail.com>
2022-10-16 18:50:01 +0100

    completed kata 4 humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,27 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  const humanYears = number;
+  let catYears = 0;
+  for (let i = 0; i < humanYears; i += 1) {
+    if (i === 1) {
+      catYears += 15;
+    } else if (i === 2) {
+      catYears += 9;
+    } else {
+      catYears += 4;
+    }
+  }
+  let dogYears = 0;
+  for (let i = 0; i < humanYears; i += 1) {
+    if (i === 1) {
+      dogYears += 15;
+    } else if (i === 2) {
+      dogYears += 9;
+    } else {
+      dogYears += 5;
+    }
+  }
+  const totalYears = [humanYears, catYears, dogYears];
+  return totalYears;
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,6 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+test("returns an array of age numbers based on formulas in the track for human, cat and dog years", () => {
+  expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+  expect(humanCatDogYears(20)).toEqual([20, 96, 114]);
+});
