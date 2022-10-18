709c530bc057f95ad8003b99cec687ed6fe8ef7a
James Goodman <jamesgoodman206@gmail.com>
2022-10-18 18:23:21 +0100

    completed kata 6 joinNames

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,19 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])
+    ).toEqual("Bart, Lisa & Maggie");
+
+    expect(
+      joinNames([
+        { name: "James" },
+        { name: "David" },
+        { name: "Ian" },
+        { name: "Boris" },
+        { name: "Scarlet" },
+      ])
+    ).toEqual("James, David, Ian, Boris & Scarlet");
+  });
 });
