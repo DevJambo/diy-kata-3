72f900e93f09d0d0fba9106e84573a61bf145ca8
James Goodman <jamesgoodman206@gmail.com>
2022-10-18 22:13:05 +0100

    finished off kata 7 getEmployerRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,5 @@
-const getEmployerRole = (employeeName, employees) => {};
-
+const getEmployerRole = (employeeName, employees) => {
+  const workerName = employees.find((person) => person.name === employeeName);
+  return workerName.role;
+};
 module.exports = getEmployerRole;
